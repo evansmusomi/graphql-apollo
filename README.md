c9fa231383eaca033104ef9292ca6464786e2aa7
Evans Musomi <evans.musomi@gmail.com>
2019-06-13 10:22:45 +0300

    chore: initialize project

---
+++ README.md
@@ -0,0 +1,9 @@
+# GraphQL Apollo Project
+
+Creating an Apollo Server with Node/Express.
+
+## Features
+
+- _Schema_: Query and mutation definitions
+- _Resolvers_: Logic to process queries and mutations
+- _Models_: Data to query and mutate
---
+++ src/models/index.js
@@ -0,0 +1,32 @@
+let users = {
+  1: {
+    id: '1',
+    name: 'Evans Musomi',
+    email: 'evans@gmail.com',
+    messageIds: [1],
+  },
+  2: {
+    id: '2',
+    name: 'Elton Maiyo',
+    email: 'elton@gmail.com',
+    messageIds: [2],
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Bye World',
+    userId: '2',
+  },
+};
+
+export default {
+  users,
+  messages,
+};
